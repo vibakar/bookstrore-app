36d6541254db186e1771b08bf3685462b21db6c2
vibakar <vibakar23@gmail.com>
2019-04-05 08:37:54 +0530

    Designed Forget Password Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm:boolean = true;
+  forgetPwdForm:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  forgetPwd() {
+  	this.loginForm = false;
+  	this.forgetPwdForm = true;
+  }
+
+  showLoginForm() {
+  	this.loginForm = true;
+  	this.forgetPwdForm = false;
+  }
 }
