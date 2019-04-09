02249bf55a2e846364571b3497204ea22f60cdc0
vibakar <vibakar23@gmail.com>
2019-04-09 06:57:55 +0530

    Making all the unit test cases success

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('bookstore-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render app name in a anchor tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bookstore-app!');
+    expect(compiled.querySelector('a').textContent).toContain('Bookstore');
   });
 });
